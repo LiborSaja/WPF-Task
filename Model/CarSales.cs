1145a676b14644cb8051d6100c09e5cc66623eda
LiborSaja <sajalibor@gmail.com>
2024-10-26 15:11:44 +0200

    Initial commit

---
+++ Model/CarSales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Model {
+    public class CarSales {
+        public int Id { get; set; } 
+        public string Model { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+        public double DPH { get; set; }
+    }
+}
